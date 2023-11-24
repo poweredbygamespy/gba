b338f3fe189c089f3b4a6bad886e32b26ed79ea1
poweredbygamespy <mipapachri@gmail.com>
2023-11-24 11:56:32 +0100

    made memcpy and memclr global

--- example.c
+++ example.c
@@ -1,5 +1,9 @@
+unsigned int a = 0x0403;
+
+//extern void memcpy(const unsigned int *src, const unsigned int* dst, const unsigned int len);
 
 int main(void) {
+	//memcpy(&a, (unsigned int*)0x04000000, sizeof(a));
 	*(unsigned int*)0x04000000 = 0x0403;
 
 	((unsigned short*)0x06000000)[120+80*240] = 0x001F;
