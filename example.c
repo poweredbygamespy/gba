779444e34c0b5d1bb9a385daf83e1e74a084debe
poweredbygamespy <mipapachri@gmail.com>
2023-11-24 22:15:15 +0100

    heap_init in crt0, malloc now shoves data in the first empty chunk

--- example.c
+++ example.c
@@ -1,21 +1,28 @@
 #include <malloc.h>
 
-const unsigned int a = 0x0403;
+__attribute__((section(".ewram")))
+unsigned int a = 0x0403;
 
 int main(void) {
-	heap_init();
 	*(unsigned int*)0x04000000 = a;
 
 	((unsigned short*)0x06000000)[120+80*240] = 0x001F;
 	((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
 	((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
 
-	int *b = malloc(10);
-	int *c = malloc(256);
-	int *d = malloc(sizeof(int));
-	free(c);
-	free(d);
-	int *e = malloc(16);
+	unsigned int *b[0x100];
+	for (int i = 0; i < 0x100; ++i) {
+		b[i] = malloc(3 * sizeof(int));
+		*b[i] = 0x12345678;
+		*(b[i] + 1) = 0x9abcdef0;
+		*(b[i] + 2) = 0xffffffff;
+	}
+
+	for (int i = 0; i < 0x40; ++i) {
+		int j = 4 * i;
+		free(b[j]);
+		free(b[j + 1]);
+	}
 
 	for(;;);
 	return 0;
--- malloc.c
+++ malloc.c
@@ -4,124 +4,140 @@ extern unsigned int __eheap_start;
 extern unsigned int __eheap_size;
 extern unsigned int __eheap_end;
 
+#define MALLOC_MIN_CHUNK_SIZE (4 * sizeof(int))
 typedef struct chunk {
 	unsigned int prev_size;
 	unsigned int size;
-	struct chunk *data;
+	struct chunk *data_or_fwd;
+	struct chunk *bck;
 } chunk_t;
 
 __attribute__((section(".ewram")))
 chunk_t *free_list = (chunk_t*)(&__eheap_start - 1);
 
-void heap_init() {
-	__eheap_start = (unsigned long int)&__eheap_size;
-}
+static inline unsigned int align(unsigned int n) { return (n + 3) & ~3u; }
 
-static inline unsigned int align(unsigned int n) { return (n + 3u) & ~3u; }
+static inline unsigned int trunc(unsigned int n) { return n & ~3u; }
+
 static inline chunk_t *get_next(chunk_t *this) {
+	chunk_t *next = (chunk_t*)((unsigned long int)this + (this->size & ~3u));
+	if (next >= (chunk_t*)((unsigned long int)&__eheap_end - sizeof(int)))
+		return 0;
 	return (chunk_t*)((unsigned long int)this + (this->size & ~3u));
 }
+
 static inline chunk_t *get_prev_free(chunk_t *this) {
 	chunk_t *prev = 0;
 	chunk_t *curr = free_list;
 	while (curr != 0 && curr < this) {
 		prev = curr;
-		curr = curr->data;
+		curr = curr->data_or_fwd;
 	}
 	return prev;
 }
 
+static inline void write_prev_size(chunk_t *empty_c) {
+	// i resign
+	*((unsigned int*)((unsigned long int)empty_c + (empty_c->size & ~3u)))
+		= empty_c->size & ~3u;
+}
+
+void heap_init() {
+	__eheap_start = (unsigned long int)&__eheap_size + 1;
+	write_prev_size(free_list);
+}
+
 void *malloc(unsigned int size) {
-	size = size >= 2 * sizeof(int) ?
-		align(size + sizeof(int)) : 2 * sizeof(int);
+	size = size >= MALLOC_MIN_CHUNK_SIZE ?
+		align(size + sizeof(int)) : MALLOC_MIN_CHUNK_SIZE;
 	
-	chunk_t *smallest_fit = 0;
-	chunk_t *curr = free_list;
-	chunk_t *prev_free = 0;
-	while (curr != 0) {
-		if ((curr->size & ~3u) >= size) {
-			if (smallest_fit == 0) {
-				smallest_fit = curr;
-			}
-			else if ((smallest_fit->size & ~3u) >= (curr->size & ~3u)) {
-				smallest_fit = curr;
-				break;
-			}
-		}
-		prev_free = curr;
-		curr = curr->data;
+	chunk_t *fit = free_list;
+	while (fit != 0 && trunc(fit->size) < size) {
+		fit = fit->data_or_fwd;
 	}
 
-	if (smallest_fit == 0) {
+	if (fit == 0)
 		return 0;
-	}
-	if (smallest_fit == free_list) {
-		prev_free = 0;
-	}
 
 	chunk_t *next;
-	if ((smallest_fit->size & ~3u) - size < 2 * sizeof(int)) { // no space for
-															   // empty chunk
-															   // after this one
-		size = smallest_fit->size & ~3u;
-		smallest_fit->size = size + 1; // previous chunk is in use
-									   // or it'd have been coalesced
+	chunk_t *prev_free;
+	chunk_t *next_free;
+	// not enough space for new empty chunk, fill up all the space
+	if ((fit->size & ~3u) - size < MALLOC_MIN_CHUNK_SIZE) {
+		fit->size = (fit->size & ~3u) + 1; // previous chunk is in use
+										   // or it'd have been coalesced
 
-		next = get_next(smallest_fit);
-		if (prev_free != 0)
-			prev_free->data = smallest_fit->data;
-		if (smallest_fit == free_list)
-			free_list = smallest_fit->data;
+		next = get_next(fit);
+
+		prev_free = fit->bck;
+		next_free = fit->data_or_fwd;
 	}
-	else { // split off new empty chunk
-		unsigned int old_size = smallest_fit->size & ~3u;
-		smallest_fit->size = size + 1;
-		next = get_next(smallest_fit);
-		if (prev_free != 0)
-			prev_free->data = next;
-		next->data = smallest_fit->data;
+	// split off new empty chunk
+	else {
+		unsigned int old_size = fit->size & ~3u;
+		fit->size = size + 1;
+
+		next = get_next(fit);
+		next->data_or_fwd = fit->data_or_fwd;
 		next->size = old_size - size;
-		if (smallest_fit == free_list)
-			free_list = next;
+		write_prev_size(next);
+
+		prev_free = fit->bck;
+		next_free = next;
 	}
+
+	if (prev_free == 0)
+		free_list = next_free;
+	else
+		prev_free->data_or_fwd = next_free;
+	next_free->bck = prev_free;
 	next->size |= 1;
 
-	return &smallest_fit->data;
+	return &fit->data_or_fwd;
 }
 
 void free(void *ptr) {
 	chunk_t *chunk = (chunk_t*)((unsigned long int)ptr - 2 * sizeof(int));
-	int coalesced = 0;
-	if (!(chunk->size & 1)) { // coalesce with previous chunk
-		coalesced = 1;
+	chunk_t *prev_free;
+	chunk_t *next_free;
+	// coalesce with previous chunk
+	if (!(chunk->size & 1)) {
 		chunk_t *prev = (chunk_t*)((unsigned long int)chunk - chunk->prev_size);
-		prev->size += chunk->size & ~3u;
+		prev->size += chunk->size;
 		chunk = prev;
+		prev_free = chunk->bck;
+		next_free = chunk->data_or_fwd;
+	}
+	else {
+		prev_free = get_prev_free(chunk);
+		if (prev_free == 0) {
+			next_free = free_list;
+			free_list = chunk;
+			chunk->bck = 0;
+		}
+		else {
+			next_free = prev_free->data_or_fwd;
+			prev_free->data_or_fwd = chunk;
+			chunk->bck = prev_free;
+		}
 	}
 
 	chunk_t *next = get_next(chunk);
-	chunk_t *prev_free = get_prev_free(chunk);
-	if (free_list == next || prev_free->data == next || (coalesced && chunk->data == next)) { // coalesce with next chunk
+	// coalesce with next chunk
+	if (next && next_free == next) {
 		chunk->size += next->size & ~3u;
-		chunk->data = next->data;
+		chunk->data_or_fwd = next->data_or_fwd;
+		if (chunk->data_or_fwd)
+			chunk->data_or_fwd->bck = chunk;
+		next_free = next->data_or_fwd;
 		next = get_next(chunk);
 	}
 
-	if (prev_free != 0) {
-		chunk->data = prev_free->data;
-		prev_free->data = chunk;
-	}
-	else {
-		if (free_list > chunk) {
-			chunk->data = free_list;
-		}
-		else if (free_list == chunk) {
-			chunk->data = free_list->data;
-		}
-		free_list = chunk;
-	}
+	chunk->data_or_fwd = next_free;
+	if (next_free)
+		next_free->bck = chunk;
 
-	if (next != (chunk_t*)((unsigned long int)&__eheap_end - sizeof(int)))
+	if (next)
 		next->size &= ~1u;
-	next->prev_size = chunk->size & ~3u;
+	write_prev_size(chunk);
 }
